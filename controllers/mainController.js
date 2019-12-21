5bca0abf188bccb51ac8e4347b64fcb0b66c94b1
skw622 <skw622vlato@gmail.com>
2019-12-21 22:48:19 +0300

    display all data from database in frontend

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,14 @@
-var testModel = require('../models/test.model')
+var productModel = require('../models/product')
 
 class mainController {
     async index(req, res) {
-        res.render('shop/index', { title: 'Express' });
+        let products = await productModel.find({})
+        let productChunks = [];
+        let chunkSize = 3;
+        for (let i = 0; i < products.length; i += chunkSize) {
+            productChunks.push(products.slice(i, i + chunkSize));
+        }
+        res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
     }
 }
 
